034fb92d589d8a1927308041063cbd87c6148a37
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-11-20 21:27:03 +0100

    Fix tests by mocking SharedPreferences

--- lib/data/services/github_service_test.dart
+++ lib/data/services/github_service_test.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: depend_on_referenced_packages
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences_platform_interface/in_memory_shared_preferences_async.dart';
+import 'package:shared_preferences_platform_interface/shared_preferences_async_platform_interface.dart';
 import 'package:wikwok/core/inject.dart';
 import 'package:wikwok/data/services/github_service.dart';
 
@@ -8,6 +10,9 @@ final _tags = ['integration'];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferencesAsyncPlatform.instance =
+      InMemorySharedPreferencesAsync.withData({});
 
   await initInject();
 
