bdcd23c34d18faf48ce4a27c22c7a3bc0813236a
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-11-21 19:16:18 +0100

    Refactor ConnectivityCubit and update build.yaml

--- lib/src/presentation/cubits/connectivity_cubit.dart
+++ lib/src/presentation/cubits/connectivity_cubit.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/services.dart';
 import 'package:injectable/injectable.dart';
-import 'package:wikwok/core.dart';
 import 'package:wikwok/presentation.dart';
 
 @injectable
@@ -11,29 +9,15 @@ import 'package:wikwok/presentation.dart';
 class ConnectivityCubit extends WCubit<List<ConnectivityResult>?> {
   ConnectivityCubit(
     this._connectivity,
-    this._exceptionHandler,
   ) : super(null);
 
   final Connectivity _connectivity;
-  final ExceptionHandler _exceptionHandler;
 
   late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
 
   Future initialize() async {
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-  }
-
-  Future<void> initConnectivity() async {
-    late List<ConnectivityResult> result;
-    try {
-      result = await _connectivity.checkConnectivity();
-    } on PlatformException catch (e) {
-      _exceptionHandler.handle(e);
-      result = [ConnectivityResult.none];
-    }
-
-    return _updateConnectionStatus(result);
   }
 
   void _updateConnectionStatus(List<ConnectivityResult> result) => emit(result);
