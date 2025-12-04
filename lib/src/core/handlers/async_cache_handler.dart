8c7302b07f1357826fcb7c1156a495164fe1452d
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-12-04 22:12:14 +0100

    Allow dynamic keys in AsyncCacheHandler

--- lib/src/core/handlers/async_cache_handler.dart
+++ lib/src/core/handlers/async_cache_handler.dart
@@ -4,10 +4,24 @@ import 'package:flutter/foundation.dart';
 
 class AsyncCacheHandler {
   @visibleForTesting
-  final Map<String, Completer> completers = {};
+  final Map<dynamic, Completer> completers = {};
 
+  /// Runs the given asynchronous [action] and caches its future.
+  ///
+  /// This method ensures that for a given [key], the [action] is only
+  /// executed once, even if `run` is called multiple times concurrently.
+  /// Subsequent calls with the same [key] while the original action is
+  /// still in progress will receive the same `Future` instance.
+  ///
+  /// - [key]: A unique key to identify the action.
+  /// - [action]: The asynchronous function to execute, which returns a `Future<T>`.
+  /// - [duration]: The `Duration` for which the result should be cached.
+  ///   - If `Duration > zero`, the result is cached for the specified duration.
+  ///   - If `Duration.zero` (the default), the cache is invalidated immediately
+  ///     after the future completes, effectively only preventing concurrent
+  ///     executions but not caching the result for future calls.
   Future<T> run<T>({
-    required String key,
+    required dynamic key,
     required Future<T> Function() action,
     Duration duration = Duration.zero,
   }) async {
@@ -34,10 +48,10 @@ class AsyncCacheHandler {
     return completer.future;
   }
 
-  void scheduleInvalidate(String key, Duration duration) =>
+  Timer scheduleInvalidate(dynamic key, Duration duration) =>
       Timer(duration, () => invalidate(key));
 
-  void invalidate(String key) => completers.remove(key);
+  void invalidate(dynamic key) => completers.remove(key);
 
   void clear() => completers.clear();
 }
