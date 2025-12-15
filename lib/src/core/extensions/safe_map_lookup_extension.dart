321281b2ef0917e880b8e18e55acc26bc24bb6be
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2025-12-15 20:28:39 +0100

    Remove `TypeError` catch block where it is never reached

--- lib/src/core/extensions/safe_map_lookup_extension.dart
+++ lib/src/core/extensions/safe_map_lookup_extension.dart
@@ -16,8 +16,6 @@ extension SafeMapLookupExtension<K, V> on Map<K, V> {
   Either<SafeMapLookupError, V> set(K key, V value) {
     try {
       return Either.right(this[key] = value);
-    } on TypeError catch (_) {
-      return Either.left(.typeMismatch);
     } catch (_) {
       return Either.left(.somethingWentWrong);
     }
