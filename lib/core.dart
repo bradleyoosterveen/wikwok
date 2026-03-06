c9934f28d597d9e3bfe277fd8471716da318f4ec
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2026-03-06 19:29:01 +0100

    Export package_info_module in core.dart

--- lib/core.dart
+++ lib/core.dart
@@ -12,4 +12,5 @@ export 'src/core/modules/async_cache_handler_module.dart';
 export 'src/core/modules/connectivity_module.dart';
 export 'src/core/modules/dio_module.dart';
 export 'src/core/modules/logger_module.dart';
+export 'src/core/modules/package_info_module.dart';
 export 'src/core/modules/shared_preferences_async_module.dart';
