a6524db8c9faf4c6d4153258d351a4e5e3a7e447
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2026-03-14 20:20:48 +0100

    Improve error message for uninitialized localization

--- lib/src/presentation/l10n.dart
+++ lib/src/presentation/l10n.dart
@@ -4,6 +4,14 @@ import 'package:wikwok/src/l10n/app_localizations.dart';
 
 AppLocalizations? _l10n;
 
-AppLocalizations get l10n => _l10n != null ? _l10n! : throw Exception();
+AppLocalizations get l10n {
+  final value = _l10n;
+  if (value == null) {
+    throw StateError(
+      'Localization has not been initialized. Call initL10n(context) before accessing l10n.',
+    );
+  }
+  return value;
+}
 
 void initL10n(BuildContext context) => _l10n = context.l10n;
